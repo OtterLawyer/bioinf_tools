fcded3a17dd666c4afba435d46806c6f5d634e55
OtterLawyer <130027058+OtterLawyer@users.noreply.github.com>
2023-10-16 12:13:33 +0900

    Change constants

--- packages/protein_tools.py
+++ packages/protein_tools.py
@@ -1,14 +1,17 @@
 from typing import Union
-aminoacid_alphabet_1to3 = {'A': 'Ala', 'R': 'Arg', 'N': 'Asn', 'D': 'Asp', 'C': 'Cys', 
+
+
+AMINOACID_ALPHABET_1TO3_MAP = {'A': 'Ala', 'R': 'Arg', 'N': 'Asn', 'D': 'Asp', 'C': 'Cys', 
 							'Q': 'Gln', 'E': 'Glu', 'G': 'Gly', 'H': 'His', 'I': 'Ile', 
 							'L': 'Leu', 'K': 'Lys', 'M': 'Met', 'F': 'Phe', 'P': 'Pro', 
 							'S': 'Ser', 'T': 'Thr', 'W': 'Trp', 'Y': 'Tyr', 'V': 'Val'}
                             
-molecular_mass = {'A': 89.094, 'R': 174.203, 'N': 132.119, 'D': 133.104, 'C': 121.154, 
+MOLECULAR_MASS_MAP = {'A': 89.094, 'R': 174.203, 'N': 132.119, 'D': 133.104, 'C': 121.154, 
                   'E': 147.131, 'Q': 146.146, 'G': 75.067, 'H': 155.156, 'I': 131.175,
                   'L': 131.175, 'K': 146.189, 'M': 149.208, 'F': 165.192, 'P': 115.132,
                   'S': 105.093, 'T': 119.119, 'W': 204.228, 'Y': 181.191, 'V': 117.148}
 
+AAS = 'ACDEFGHIKLMNPQRSTVWY'
 
 def is_prot(prot: str) -> bool:
     """
@@ -19,10 +22,9 @@ def is_prot(prot: str) -> bool:
         bool if sequence is correct
         ValueError('Please  check proteins sequences') if there were wrong symbols
     """
-    aas = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'}
     prot = prot.upper()
-    uniq_aas = set(prot)
-    aa_test = (uniq_aas <= aas)
+    uniq_AAS = set(prot)
+    aa_test = (uniq_AAS <= set(AAS))
     if aa_test == 0:
         raise ValueError('Please  check proteins sequences')
     return True
@@ -39,8 +41,8 @@ def convert_1to3(prot: str) -> str:
     output = ''
     if len(prot) > 0:
         for i in prot:
-            if i in aminoacid_alphabet_1to3:
-                output += aminoacid_alphabet_1to3[i]
+            if i in AMINOACID_ALPHABET_1TO3_MAP:
+                output += AMINOACID_ALPHABET_1TO3_MAP[i]
             else:
                 raise ValueError('Input format: aminoacids in uppercase 1-letter symbols')
     return output
@@ -57,10 +59,10 @@ def calculate_mm(prot: str) -> float:
     prot_seq = set(prot)
     output = 0
     if len(prot) == 1:
-        output = molecular_mass[prot]
+        output = MOLECULAR_MASS_MAP[prot]
     else:
         for i in prot_seq:
-            output += prot.count(i) * molecular_mass[i]
+            output += prot.count(i) * MOLECULAR_MASS_MAP[i]
     output -= 18.0153*(len(prot)-1)
     return round(output,3)
 
@@ -95,14 +97,13 @@ def count_aa_content(prot: str) -> dict:
     Return: aa_content (dict) - dict of aminoacids and their quantity in protein
     """
 
-    aas = 'ACDEFGHIKLMNPQRSTVWY'
     prot = prot.upper()
     aa_counter = [0] * 20
     for i in range(len(prot)):
-        n = aas.index(prot[i])
+        n = AAS.index(prot[i])
         aa_counter[n] += 1
 
-    aa_content = dict(zip(list(aas), aa_counter))
+    aa_content = dict(zip(list(AAS), aa_counter))
     return aa_content
 
 
@@ -137,7 +138,7 @@ def protein_tools (function : str, *prots : str) -> Union[int, list, str]:
       -built in fuction 'is_prot': check whether the inputted sequence is a peptide 
       -'count_length': count the length of the sequence
       -'count_nucleotide_length': count the length of the coding nucleotide sequence of the inputted sequence
-      -'count_molecular_mass': count the molecular mass of the sequence
+      -'count_MOLECULAR_MASS_MAP': count the molecular mass of the sequence
       -'convert_1_to_3': convert 1-letter input style into 3-letter and vice versa
       -'show_content': show the aminoacid content of the sequence
       -''count_extinction_280nm': Count extinction coefficient 280nm
@@ -150,7 +151,7 @@ def protein_tools (function : str, *prots : str) -> Union[int, list, str]:
 
     """
     functions = {'count_length':count_aa_length, 'count_nucleotide_length':count_nucl_length,
-                 'count_molecular_mass':calculate_mm, 'show_content':count_aa_content, 'convert_1_to_3':convert_1to3,
+                 'count_MOLECULAR_MASS_MAP':calculate_mm, 'show_content':count_aa_content, 'convert_1_to_3':convert_1to3,
                   'count_extinction_280nm':count_extinction_280nm }
     protein = []
     for prot in prots:
