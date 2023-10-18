51a5a61ca781fa624c05f7cb882794e6137f72d3
OtterLawyer <130027058+OtterLawyer@users.noreply.github.com>
2023-10-19 01:20:59 +0900

    Add change_fasta_start_pos function

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -30,3 +30,25 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = 'on
             f.write(names[i])
             out_fasta = out[i+1] + '\n'
             f.write(out_fasta)
+
+def change_fasta_start_pos(input_fasta: str, shift: int = 1, output_fasta:str = None) -> None:
+    """
+    Takes in fasta file with one oneline genome and shifts starting position based on shift variable.
+    Arguments:
+    -input_fasta (str) - input file name.
+    -shift (int) - new starting position.
+    -output_fasta (str) - name of output file. If none is given will output file will have 'shift' before input_fasta name.
+    Return: None
+    """
+    shift_genome = ''
+    if output_fasta is None:
+        output_fasta = 'shift_' + input_fasta
+    else:
+        output_fasta += '.fasta'
+    with open(input_fasta) as f:
+        name = f.readline()
+        genome = f.readline().strip()
+        shift_genome = genome[shift:] + genome[:shift]
+    with open(output_fasta, 'w') as f:
+        f.write(name)
+        f.write(shift_genome)
