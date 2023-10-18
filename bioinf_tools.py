408612904f392f77c60f65d1f1db5369552fce33
OtterLawyer <130027058+OtterLawyer@users.noreply.github.com>
2023-10-19 01:25:55 +0900

    Update bioinf_tools.py

--- bioinf_tools.py
+++ bioinf_tools.py
@@ -2,3 +2,5 @@ from packages.dna_tools import run_dna_rna_tools as na_tools
 from packages.protein_tools import protein_tools as p_tools
 from packages.fastq_tools import fastq_tools as fq_tools
 from packages.fastq_tools import write_fastq as w_fq
+from bio_files_processor.py import convert_multiline_fasta_to_oneline
+from bio_files_processor.py import change_fasta_start_pos
